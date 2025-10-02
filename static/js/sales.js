b70ff326ea864ce8397998715d524f0ba4fd8378
AoS <aos.fontana.dev@gmail.com>
2025-10-02 17:56:38 -0300

    estilado
    Se modificó svg de sales.js a static. Estilado de client details

--- static/js/sales.js
+++ static/js/sales.js
@@ -2,16 +2,8 @@ import { SalesFetcher } from "./requests/fetchers/sale_fetcher.js";
 import { setInputQuantity } from "./sale_quant.js";
 import { Modal } from "./modal.js";
 
+const trashIconSVG = window.trashIconSVG
 
-const trashIconSVG = `
-<svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M10 11V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M14 11V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M4 7H20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M6 7H12H18V18C18 19.6569 16.6569 21 15 21H9C7.34315 21 6 19.6569 6 18V7Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-`;
 
 const buildRow = (row, data) => {
     const cellsConfig = [
@@ -20,7 +12,6 @@ const buildRow = (row, data) => {
             class: 'editable-cell quantity-cell',
             attributes: {
                 'data-sale-detail-id': data.id,
-                // 'ondblclick': 'setInputQuantity(this)'
             }
         },
         {
@@ -44,7 +35,7 @@ const buildRow = (row, data) => {
                         'data-sale-detail-id': data.id,
                         'title': 'Eliminar',
                     },
-                    content: trashIconSVG
+                    content: `<img src="${trashIconSVG}" width="20" height="20" alt="Delete" />`
                 }
             ]
         }
